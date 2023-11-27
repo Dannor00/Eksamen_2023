1dd7930bd9aeb51cdf42f9524ee58533ec67ea2c
Daniel Hatlem <143505870+Dannor00@users.noreply.github.com>
2023-11-27 15:21:18 +0100

    pusher endringer til main

---
+++ include/CalculateTime.hpp
@@ -0,0 +1,24 @@
+
+#ifndef EKSAMEN_2023_CALCULATETIME_HPP
+#define EKSAMEN_2023_CALCULATETIME_HPP
+
+#include <chrono>
+
+class TimeUtils {
+public:
+    TimeUtils() : startTime(std::chrono::high_resolution_clock::now()) {}
+
+    float calculateDeltaTime() {
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(currentTime - startTime);
+        startTime = currentTime;
+        return static_cast<float>(duration.count()) * 1e-6f;
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point startTime;
+};
+
+
+
+#endif //EKSAMEN_2023_CALCULATETIME_HPP
