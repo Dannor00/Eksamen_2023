413c6d152a32a7fc2cd9e0925b6b98968ff19b2d
Daniel Hatlem <143505870+Dannor00@users.noreply.github.com>
2023-11-29 14:48:39 +0100

    fått brikkene til å kolidere med hverandre men ser ikke blokkene på skjermen

--- include/game.hpp
+++ include/game.hpp
@@ -6,30 +6,41 @@
 #include <random>
 #include "grid.hpp"
 #include "../src/blocks.cpp"
+#include "threepp/threepp.hpp"
 
 class Game {
 public:
     Game();
     Block GetRandomBlock();
     static std::vector<Block> GetAllBlocks();
-    void Update(Scene &scene, float d);
-    void Draw(threepp::Scene& scene);
+    void Update(threepp::Scene &scene, float deltaTime);
+    void Draw(threepp::Scene &scene);
     void moveCurrentBlock(int rows, int columns);
     void RotateBlock();
-    bool IsBlockOutside(const Block& block, int rows, int columns);
+
+
     Grid grid;
 
 private:
+    struct LockedBlock {
+        LockedBlock(Position position);
+
+        int blockId;
+        Position position;
+    };
+    bool IsBlockOutside(const Block &block, int rows, int columns);
     std::vector<Block> blocks;
     Block currentBlock;
     Block nextBlock;
+    std::vector<LockedBlock> lockedBlocks;  // Added to store locked blocks
     std::random_device rd;
     std::mt19937 gen;
     const float blockFallInterval = 1.0f;
     float elapsedSinceLastFall = 0.0f;
 
+    void LockBlock();           // Added
+    void RedrawLockedBlocks();  // Added
     bool IsCollision(const Block &block, int rows, int columns);
 };
 
 #endif // EKSAMEN_2023_GAME_HPP
-
--- src/game.cpp
+++ src/game.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "../include/game.hpp"
 
 
@@ -27,15 +28,22 @@ void Game::Update(Scene &scene, float deltaTime) {
     // Automatic downward movement based on elapsed time
     elapsedSinceLastFall += deltaTime;
 
-    // Check if it's time to move the block down
     if (elapsedSinceLastFall >= blockFallInterval) {
         elapsedSinceLastFall = 0.0f;
 
         // Attempt to move the block down
         if (!IsBlockOutside(currentBlock, 1, 0) && !IsCollision(currentBlock, 1, 0)) {
             currentBlock.Move(1, 0);
+        } else {
+            // Block cannot move down anymore, lock it in place
+            std::cout << "Attempting to lock the block." << std::endl;
+            LockBlock();
+
+            // Reset the elapsed time to avoid continuous locking
+            elapsedSinceLastFall = 0.0f;
         }
     }
+
     if (!IsBlockOutside(currentBlock, -1, 0) && !IsCollision(currentBlock, -1, 0)) {
         currentBlock.Move(-1, 0);
     }
@@ -53,6 +61,7 @@ void Game::Update(Scene &scene, float deltaTime) {
     }
 
     // Draw the game scene
+    std::cout << "Drawing the game scene." << std::endl;
     currentBlock.Draw(scene);
 }
 
@@ -63,7 +72,25 @@ void Game::Draw(threepp::Scene &scene) {
 }
 
 bool Game::IsCollision(const Block &block, int rows, int columns) {
-    return false;
+    std::vector<Position> newPositions = block.GetCellPositionsAfterMove(rows, columns);
+    // Check if any of the new positions are outside the grid boundaries
+    for (const auto& newPos : newPositions) {
+        // Allow blocks to reach the topmost position of the grid
+        if (newPos.row < 0) {
+            continue; // Allow block to move to the top
+        }
+        // Allow blocks to reach the leftmost position of the grid
+        if (newPos.column < 0) {
+            continue; // Allow block to move to the left
+        }
+        if (newPos.row >= grid.numRows || newPos.column >= grid.numCols) {
+            return true; // Collision with right or bottom boundary
+        }
+        if (grid.grid[newPos.row][newPos.column] != 0) {
+            return true; // Collision with existing occupied cell in the grid
+        }
+    }
+    return false; // Block is within the grid
 }
 
 bool Game::IsBlockOutside(const Block& block, int rows, int columns) {
@@ -99,4 +126,36 @@ void Game::moveCurrentBlock(int rows, int columns) {
 
 void Game::RotateBlock() {
     currentBlock.Rotate();
+}
+void Game::LockBlock() {
+    // Get the current position of the block
+    std::vector<Position> blockPositions = currentBlock.GetCellPositions();
+
+    // Lock the block in place and add it to the list of locked blocks
+    for (const auto &pos : blockPositions) {
+        int lockedRow = pos.row;
+        int lockedColumn = pos.column;
+        grid.grid[lockedRow][lockedColumn] = currentBlock.id;  // Assuming id represents the block's unique identifier
+
+        // Create a LockedBlock structure and add it to the list
+        LockedBlock lockedBlock(Position(0, 0));
+        lockedBlock.blockId = currentBlock.id;
+        lockedBlock.position = pos;
+        lockedBlocks.push_back(lockedBlock);
+    }
+
+    // Generate a new block
+    currentBlock = GetRandomBlock();
+}
+
+// Function to redraw the locked blocks
+void Game::RedrawLockedBlocks() {
+    for (const auto &lockedBlock : lockedBlocks) {
+        grid.grid[lockedBlock.position.row][lockedBlock.position.column] = lockedBlock.blockId;
+        // You may need additional code here to redraw the block visually in your game.
+    }
+}
+
+Game::LockedBlock::LockedBlock(Position position) : position(position) {
+
 }
